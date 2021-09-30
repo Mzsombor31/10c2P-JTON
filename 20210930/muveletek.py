4fb6e692fa5c3de7ac62b4d61cc6e8804d86fa42
Molnár Zsombor László <molnar.zsombor@sulinet.csmszki>
2021-09-30 14:08:44 +0200

    orai munka

--- 20210930/muveletek.py
+++ 20210930/muveletek.py
@@ -1,6 +1,62 @@
 #kerekítés
-print(round(3.1415, 3)) #round(mit, tizedesek száma)
 #hatványozás
-print(pow(2, 8)) #pow(alap, kitevő)
-#abszolút érték
-print(abs(mit))
+#print(pow(2, 8)) #pow(alap, kitevő)
+#abszolút érték print(abs(mit))
+#print(abs(-12))
+#print(min(12,7,9,45,3))
+#print(max(12,7,9,45,3))
+# pi 
+'''
+import math 
+a=math.pi
+print(a)
+#'''
+#from math import * #program elején helyezzük el
+#a=pi
+#print(a)
+#négyzetgyök -sqrt
+#print(sqrt(16))  #math.sqrt(miből vonok gyököt)
+# felfelé kerekítés - math.ceil(mit kerekítek)
+#print(floor(3.56))
+#print(ceil(3.56))
+#lefelé kerekítés - math.floor(mit)
+#print(floor(3.56))
+
+
+# 1. négyzet kerület, terület
+a=float(input("Kérem a négyzet oldalát! a="))
+k=4*a
+t=pow(a,2)
+print("a(z)", a , "egység oldalú négyzet kerülete", k , "területe", t)
+
+# 2. téglalap terület, kerület
+
+
+
+# 3. kör terület(2*r*PI), kerület (r^2*PI)
+
+
+
+# 4. kocka térfogata (V=a^3), felszíne (A=a^2)
+
+
+
+# 5. derékszögű háromszög 2 befogóját kérd be, add meg az átfogót!
+# c=négyzetgyök(a^2+b^2)
+
+befogo1 = float(input("Addja meg az első befogót!"))
+befog2 = float(input("Addja meg a második befogót"))
+atfogo = input(10)
+def
+
+if befogo1 + befogo2 > atfogo
+    print("Ez háromszög")
+elif: befogo1 + befogo2 < atfogo 
+    print("Ez nem háromszög")
+else: 
+    print("Ezekkel a számokkal nem lehet elvégezni a műveletet!")
+    
+    
+
+
+
