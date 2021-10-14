2eb6310f62b8b7a06704dc0d32461b2ffcc888c0
Molnár Zsombor László <molnar.zsombor@sulinet.csmszki>
2021-10-14 09:38:52 +0200

    orai munka

---
+++ 20211014/20211014.py
@@ -0,0 +1,51 @@
+def feladat1():
+
+    for i in range(1,21):
+        print(i,end="")
+    print()
+    for i in range(1,21):
+        print(i)
+
+
+
+
+#itt kezdődik a főprogram
+#feladat1()
+
+def feladat2():
+
+    for i in range(15,93):
+        print(i,end=" ")
+    
+#feladat2()
+
+def feladat3():
+    for i in range(1,31):
+        if i%2 == 0:
+            print(i, end=" ")
+
+#feladat3()
+
+def feladat4():     #HF
+    print()
+
+def feladat5():     #HF
+    print()
+
+
+def feladat10():
+    for i in range(100, -100, -1):
+        if i%9 == 0:
+            print(i, end=" ")
+
+#feladat10()
+
+def feladat8():
+    for i in range(30, 101, 9):
+        print(i, end=" , ")
+
+feladat8()
+
+
+
+
